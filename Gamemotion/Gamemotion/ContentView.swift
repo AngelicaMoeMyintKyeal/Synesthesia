7a456273845dabce46602e16674d17a600536364
Htet Myat Moe Myint Kyeal <htetmyak.m.m.kyal@gmail.com>
2023-02-15 11:02:12 +0100

    created a new project

---
+++ Gamemotion/Gamemotion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Gamemotion
+//
+//  Created by Htet Myat Moe Myint Kyeal on 15/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gamemotion/Gamemotion/GamemotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamemotionApp.swift
+//  Gamemotion
+//
+//  Created by Htet Myat Moe Myint Kyeal on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GamemotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
