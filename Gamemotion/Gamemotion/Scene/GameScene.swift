3a2aae3731f7fd674789b3cec8375b28b6c78685
Htet Myat Moe Myint Kyeal <htetmyak.m.m.kyal@gmail.com>
2023-02-21 14:59:39 +0100

    gave physics to hero

---
+++ Gamemotion/Gamemotion/Scene/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  Gamemotion
+//
+//  Created by Htet Myat Moe Myint Kyeal on 21/02/23.
+//
+
+import Foundation
+import SpriteKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var hero = SKSpriteNode()
+    var heroTexture = SKTexture(imageNamed: "Character")
+    
+    enum bitMasks: UInt32 {
+        case hero = 0b1
+        case ground = 0b10
+    }
+    
+    override func didMove(to view: SKView) {
+        addHero()
+    }
+    
+    func addHero() {
+        hero = childNode(withName: "hero") as! SKSpriteNode
+        hero.physicsBody = SKPhysicsBody(texture: heroTexture, size: hero.size)
+        hero.physicsBody?.categoryBitMask = bitMasks.hero.rawValue
+        hero.physicsBody?.contactTestBitMask = bitMasks.ground.rawValue
+        hero.physicsBody?.collisionBitMask = bitMasks.ground.rawValue
+    }
+}
