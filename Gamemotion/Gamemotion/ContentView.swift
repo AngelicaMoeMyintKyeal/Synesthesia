eabad4632db9177b470beb8ae6e4a6378d1caf85
Htet Myat Moe Myint Kyeal <htetmyak.m.m.kyal@gmail.com>
2023-02-23 10:13:25 +0100

    full screen size adjustment for scene

--- Gamemotion/Gamemotion/ContentView.swift
+++ Gamemotion/Gamemotion/ContentView.swift
@@ -10,11 +10,18 @@ import SpriteKit
 
 struct ContentView: View {
     @StateObject var gameController = GameController(SKScene: SKScene(fileNamed: "RageScene")!)
-    var scene = SKScene(fileNamed: "RageScene")!
+    var rageScene = SKScene(fileNamed: "RageScene")!
+    
+    var scene: SKScene {
+        let scene = self.rageScene
+            scene.size = CGSize(width: 1200, height: 600)
+            scene.scaleMode = .fill
+            return scene
+        }
     
     var body: some View {
         SpriteView(scene: self.scene)
-            .frame(width: 1200, height: 600)
+            .ignoresSafeArea()
     }
 }
 
